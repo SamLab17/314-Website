d217fac2593b55970a535ef8e6ce8abd896e9caf
Samuel Laberge <samuel.laberge@utexas.edu>
2020-09-02 11:21:14 -0600

    Adds home directory prefix to paths

--- update_website.sh
+++ update_website.sh
@@ -1,16 +1,16 @@
 GIT_REPO_URL="https://github.com/SamLab17/314-Website.git"
 
 echo "Creating backup of public_html"
-cp -r public_html public_html_backup
+cp -r ~/public_html ~/public_html_backup
 
 echo "Pulling website repo"
-git clone ${GIT_REPO_URL} 314Website
+git clone ${GIT_REPO_URL} ~/314Website
 
 echo "Copying files into public_html"
-cp -r 314Website/public/* public_html/
+cp -r ~/314Website/public/* ~/public_html/
 
 echo "Fixing file & directory permissions"
-(cd public_html; find . -type f -exec chmod 644 {} \; ; find . -type d -exec chmod 755 {} \;)
+(cd ~/public_html; find . -type f -exec chmod 644 {} \; ; find . -type d -exec chmod 755 {} \;)
 
 echo "Cleaning up..."
-rm -rf 314Website
+rm -rf ~/314Website
